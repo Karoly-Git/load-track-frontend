5e2146ab58c5a5803dec7ef38a358cea642c6d96
Karoly Hornyak <karoly.webdev@gmail.com>
2026-01-06 12:39:20 +0000

    fix(modal): close modal on Escape and control focus

--- src/components/ui/modal/Modal.jsx
+++ src/components/ui/modal/Modal.jsx
@@ -1,13 +1,25 @@
+import { useEffect } from "react";
 import "./Modal.css";
 
 export default function Modal({ isOpen, onClose, children }) {
+    useEffect(() => {
+        if (!isOpen) return;
+
+        const handleKeyDown = (e) => {
+            if (e.key === "Escape") {
+                onClose();
+                document.activeElement.blur();
+            };
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+        return () => window.removeEventListener("keydown", handleKeyDown);
+    }, [isOpen, onClose]);
+
     if (!isOpen) return null;
 
     const handleOverlayClick = () => onClose();
-
-    const stopPropagation = (e) => {
-        e.stopPropagation();
-    };
+    const stopPropagation = (e) => e.stopPropagation();
 
     return (
         <div className="modal-overlay" onClick={handleOverlayClick}>
@@ -15,7 +27,6 @@ export default function Modal({ isOpen, onClose, children }) {
                 <button className="modal-close-btn" onClick={onClose}>
                     ✕
                 </button>
-
                 {children}
             </div>
         </div>
